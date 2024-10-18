572e35bd8afedc98431a88c5762c48b5488f4707
Roman Iakymovich <romaniakymovich@MacBook-Pro-Roman.local>
2024-10-18 13:18:43 +0300

    +

---
+++ src/components/ContactList/ContactList.jsx
@@ -0,0 +1,23 @@
+import Contact from '../Contact/Contact.jsx'
+import css from './ContactList.module.css'
+import { useSelector } from 'react-redux';
+import { selectFilteredContacts } from '../../redux/contactSlice.js';
+
+const ContactList = () => {
+  const contacts = useSelector(selectFilteredContacts);
+  if (!contacts || contacts.length === 0) {
+    return <p>No contacts found.</p>;
+  }
+
+  return (
+    <ul className={css.list}>
+      {contacts.map(contact => (
+        <li key={contact.id} className={css.contact}>
+          <Contact contact={contact}/>
+        </li>
+      ))}
+    </ul>
+  )
+}
+
+export default ContactList
